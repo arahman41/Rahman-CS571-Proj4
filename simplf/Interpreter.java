6609f8c3d1c603e5acbf3aeeeef4f73903e74719
Ateeq Rahman <arahman41@binghamton.edu>
2025-04-11 21:00:58 -0400

    simpf files

--- simplf/Interpreter.java
+++ simplf/Interpreter.java
@@ -12,18 +12,16 @@ public class Interpreter {
         if (stmt.initializer != null) {
             value = evaluate(stmt.initializer, env);
         }
-        env.define(stmt.name, value);
+        env.define(stmt.name.lexeme, value);
     }
 
     public void visitBlockStmt(Stmt.Block stmt, Environment env) {
         Environment blockEnv = new Environment(env);
-        for (Stmt statement : stmt.statements) {
-            visitStmt(statement, blockEnv);
-        }
+        executeBlock(stmt.statements, blockEnv);
     }
 
     public void visitIfStmt(Stmt.If stmt, Environment env) {
-        Object condition = evaluate(stmt.condition, env);
+        Object condition = evaluate(stmt.cond, env); // Fixed: stmt.cond
         if (isTruthy(condition)) {
             visitStmt(stmt.thenBranch, env);
         } else if (stmt.elseBranch != null) {
@@ -32,41 +30,59 @@ public class Interpreter {
     }
 
     public void visitWhileStmt(Stmt.While stmt, Environment env) {
-        while (isTruthy(evaluate(stmt.condition, env))) {
+        while (isTruthy(evaluate(stmt.cond, env))) { // Fixed: stmt.cond
             visitStmt(stmt.body, env);
         }
     }
 
     public void visitFunctionStmt(Stmt.Function stmt, Environment env) {
         SimplfFunction function = new SimplfFunction(stmt, env);
-        env.define(stmt.name, function);
+        env.define(stmt.name.lexeme, function);
     }
 
     // Expression visitors
     public Object visitVarExpr(Expr.Variable expr, Environment env) {
-        return env.get(expr.name);
+        return env.get(expr.name.lexeme);
     }
 
     public Object visitAssignExpr(Expr.Assign expr, Environment env) {
         Object value = evaluate(expr.value, env);
-        env.assign(expr.name, value);
+        env.assign(expr.name.lexeme, value);
         return value;
     }
 
     public Object visitCallExpr(Expr.Call expr, Environment env) {
         Object callee = evaluate(expr.callee, env);
-        if (!(callee instanceof SimplfFunction)) {
-            throw new RuntimeException("Can only call functions.");
+        if (!(callee instanceof SimplfCallable)) {
+            throw new RuntimeError(expr.paren, "Can only call functions.");
         }
-        SimplfFunction function = (SimplfFunction) callee;
+        SimplfCallable function = (SimplfCallable) callee;
         List<Object> arguments = new ArrayList<>();
-        for (Expr arg : expr.arguments) {
+        for (Expr arg : expr.args) {
             arguments.add(evaluate(arg, env));
         }
         if (arguments.size() != function.arity()) {
-            throw new RuntimeException("Expected " + function.arity() + " arguments but got " + arguments.size());
+            throw new RuntimeError(expr.paren,
+                    "Expected " + function.arity() + " arguments but got " + arguments.size());
         }
         return function.call(this, arguments);
+    }
+
+    // Expression statement visitor
+    public Void visitExprStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expr, globalEnv);
+        return null;
+    }
+
+    // Execute methods
+    public void execute(Stmt stmt, Environment env) {
+        visitStmt(stmt, env);
+    }
+
+    public void executeBlock(List<Stmt> statements, Environment env) {
+        for (Stmt statement : statements) {
+            execute(statement, env);
+        }
     }
 
     // Helper methods
@@ -81,8 +97,11 @@ public class Interpreter {
             visitWhileStmt((Stmt.While) stmt, env);
         } else if (stmt instanceof Stmt.Function) {
             visitFunctionStmt((Stmt.Function) stmt, env);
-        }else if (stmt instanceof Stmt.Expression) {
-            evaluate(((Stmt.Expression) stmt).expression, env);
+        } else if (stmt instanceof Stmt.Expression) {
+            visitExprStmt((Stmt.Expression) stmt);
+        } else if (stmt instanceof Stmt.Print) {
+            Object value = evaluate(((Stmt.Print) stmt).expr, env);
+            System.out.println(value);
         }
     }
 
@@ -93,12 +112,55 @@ public class Interpreter {
             return visitAssignExpr((Expr.Assign) expr, env);
         } else if (expr instanceof Expr.Call) {
             return visitCallExpr((Expr.Call) expr, env);
+        } else if (expr instanceof Expr.Literal) {
+            return ((Expr.Literal) expr).val;
+        } else if (expr instanceof Expr.Grouping) {
+            return evaluate(((Expr.Grouping) expr).expression, env);
+        } else if (expr instanceof Expr.Unary) {
+            Expr.Unary unary = (Expr.Unary) expr;
+            Object right = evaluate(unary.right, env);
+            switch (unary.op.type) {
+                case MINUS: return -(double) right;
+                case BANG: return !isTruthy(right);
+            }
+        } else if (expr instanceof Expr.Binary) {
+            Expr.Binary binary = (Expr.Binary) expr;
+            Object left = evaluate(binary.left, env);
+            Object right = evaluate(binary.right, env);
+            switch (binary.op.type) {
+                case PLUS:
+                    if (left instanceof Double && right instanceof Double) {
+                        return (double) left + (double) right;
+                    }
+                    if (left instanceof String || right instanceof String) {
+                        return left.toString() + right.toString();
+                    }
+                    throw new RuntimeError(binary.op, "Operands must be numbers or strings.");
+                case MINUS: return (double) left - (double) right;
+                case STAR: return (double) left * (double) right;
+                case SLASH:
+                    if ((double) right == 0) {
+                        throw new RuntimeError(binary.op, "Division by zero.");
+                    }
+                    return (double) left / (double) right;
+                case GREATER: return (double) left > (double) right;
+                case GREATER_EQUAL: return (double) left >= (double) right;
+                case LESS: return (double) left < (double) right;
+                case LESS_EQUAL: return (double) left <= (double) right;
+                case EQUAL_EQUAL: return isEqual(left, right);
+                case BANG_EQUAL: return !isEqual(left, right);
+            }
+        } else if (expr instanceof Expr.Logical) {
+            Expr.Logical logical = (Expr.Logical) expr;
+            Object left = evaluate(logical.left, env);
+            if (logical.op.type == TokenType.OR) {
+                if (isTruthy(left)) return left;
+            } else {
+                if (!isTruthy(left)) return left;
+            }
+            return evaluate(logical.right, env);
         }
-        return null; // Placeholder for other expressions
-    }
-
-    public void execute(Stmt stmt, Environment env) {
-        visitStmt(stmt, env);
+        return null;
     }
 
     private boolean isTruthy(Object value) {
@@ -107,8 +169,17 @@ public class Interpreter {
         return true;
     }
 
-    public Void visitExprStmt(Stmt.Expression stmt) {
-        evaluate(stmt.expr, env);
-        return null;
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+
+    public void interpret(List<Stmt> statements) {
+        try {
+            executeBlock(statements, globalEnv);
+        } catch (RuntimeError error) {
+            Simplf.runtimeError(error);
+        }
     }
 }
--- simplf/SimplfFunction.java
+++ simplf/SimplfFunction.java
@@ -14,39 +14,33 @@ public class SimplfFunction implements SimplfCallable {
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         if (arguments.size() != arity()) {
-            throw new RuntimeError(declaration.name,
-                    "Expected " + arity() + " arguments but got " + arguments.size() + ".");
+            throw new RuntimeError(
+                    declaration.name,
+                    "Expected " + arity() + " arguments but got " + arguments.size() + "."
+            );
         }
 
-        Environment env = new Environment(closure);
-        for (int i = 0; i < declaration.parameters.size(); i++) {
-            env.define(declaration.parameters.get(i).lexeme, arguments.get(i));
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
 
         try {
-            interpreter.executeBlock(declaration.body, env);
+            interpreter.executeBlock(declaration.body, environment);
         } catch (Return returnValue) {
             return returnValue.value;
         }
-        return null; // Default return for no explicit 'return' statement
+
+        return null;
     }
 
     @Override
     public int arity() {
-        return declaration.parameters.size();
+        return declaration.params.size();
     }
 
     @Override
     public String toString() {
-        return "<fn " + declaration.name.lexeme + ">";
-    }
-}
-
-public class Return extends RuntimeException {
-    final Object value;
-
-    public Return(Object value) {
-        super(null, null, false, false);
-        this.value = value;
+        return "<fn " + declaration.name.lexeme + "/" + arity() + ">";
     }
 }
